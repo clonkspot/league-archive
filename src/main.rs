6fbaf455d6aebdcb0f621e85be7d826b6e35ae17
Lukas Werling <lukas.werling@gmail.com>
2016-04-03 21:46:08 +0200

    Pass sqlite db path via command line

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate league_archive;
+extern crate clap;
 
 use std::env;
 use std::process::exit;
+use std::path::Path;
+use clap::App;
 use league_archive::*;
 
 fn get_env(name: &str) -> String {
@@ -15,7 +18,26 @@ fn get_env(name: &str) -> String {
 }
 
 fn main() {
-    let mut archiver = Archiver::new(&get_env("MYSQL_URL"), &get_env("SQLITE_DB")).unwrap();
+    let mysql_url = get_env("MYSQL_URL");
+
+    let matches = App::new("League Archiver")
+        .about("Creates a copy of the league MySQL tables as SQLite database")
+        .args_from_usage(
+            "<path> 'Output path of the SQLite database'
+            -f      'Override the database if it exists'")
+        .get_matches();
+
+    let sqlite_path = Path::new(matches.value_of("path").unwrap());
+    if sqlite_path.exists() {
+        if matches.is_present("f") {
+            std::fs::remove_file(sqlite_path).unwrap();
+        } else {
+            println!("File {} does already exist, use -f to overwrite.", sqlite_path.to_str().unwrap());
+            std::process::exit(1);
+        }
+    }
+
+    let mut archiver = Archiver::new(&mysql_url, &sqlite_path.to_str().unwrap()).unwrap();
     let result = archiver.copy_all().unwrap();
     println!("Done, copied {} rows.", result);
 }
